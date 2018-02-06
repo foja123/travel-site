27914cdc06d0dfd48615410963a06db42bdfb64d
Andrea Serafin <andrea.serafin@qmi.it>
2018-02-06 11:26:22 +0100

    clear javascript

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,1 @@
-import Person from './modules/Person';
-
-
-var andrea = new Person("Andrea", "Serafin");
-
-andrea.greet();
-
-
 
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -70,50 +70,5 @@
 "use strict";
 
 
-var _Person = __webpack_require__(1);
-
-var _Person2 = _interopRequireDefault(_Person);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var andrea = new _Person2.default("Andrea", "Serafin");
-
-andrea.greet();
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Person = function () {
-    function Person(name, surname) {
-        _classCallCheck(this, Person);
-
-        this.name = name;
-        this.surname = surname;
-    }
-
-    _createClass(Person, [{
-        key: "greet",
-        value: function greet() {
-            console.log("Hello " + this.name + ' ' + this.surname);
-        }
-    }]);
-
-    return Person;
-}();
-
-exports.default = Person;
-
 /***/ })
 /******/ ]);
